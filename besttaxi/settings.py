62ddd4c96aae0821267fb4eff8d6c4c6ae8a8d3c
rmad17 <souravbasu17@gmail.com>
2016-06-18 00:24:27 +0530

    Added settings for google maps and price config for uber banagalore

--- besttaxi/settings.py
+++ besttaxi/settings.py
@@ -114,3 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# =====================================================
+# 3rd Party
+# ======================================================
+
+GOOGLE_MAPS_DIRECTIONS_BASE_API_URL = 'https://maps.googleapis.com/maps/api/directions/'  # noqa
+GOOGLE_MAPS_DIRECTIONS_BASE_API_URL_OUTPUT_FORMAT = 'json'
---
+++ uber/fares.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 rmad #
+# Distributed under terms of the MIT license.
+
+import configparser
+
+config = configparser.ConfigParser()
+config['bengaluru'] = {
+        'ubergo': {'base': 35, 'per_min': 1, 'per_km': 7, 'min_fare': 50},
+        'uberx': {'base': 40, 'per_min': 1, 'per_km': 8, 'min_fare': 75},
+        'uberxl': {'base': 80, 'per_min': 1, 'per_km': 17, 'min_fare': 80}
+        }
